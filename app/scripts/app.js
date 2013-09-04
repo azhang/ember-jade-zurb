49c5e3a44380804d455832f460ee99c2d4a63e27
Aaron Zhang <aaronzhang1@gmail.com>
2013-09-04 16:43:14 -0400

    ember, jade, foundation

---
+++ app/scripts/app.js
@@ -0,0 +1,15 @@
+var EmberApp = window.EmberApp = Ember.Application.create();
+
+EmberApp.IndexView = Ember.View.extend({
+ didInsertElement: function() {
+  this.$().foundation();
+ }
+})
+
+/* Order and include as you please. */
+require('scripts/controllers/*');
+require('scripts/store');
+require('scripts/models/*');
+require('scripts/routes/*');
+require('scripts/views/*');
+require('scripts/router');
